6c12009ab0d54164a47e51acd0b9be7f21b2bf2e
Matthew Ishii <ishiimatthew@gmail.com>
2018-06-14 04:16:42 -0700

    Added CSS and links to Portfolio.html. Added jQuery for styling.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
-function scrollToLearnMore(){
-   
-}
+$(function(){
+
+   $('.slideDownMain').slideDown(2000);
+   $('.fadeInMain').delay(2000).fadeIn(2000);
+
+   $('.selectionButton').click(function(){
+
+      $('.fadeInNext').slideDown(1000);
+   });
+
+
+});
