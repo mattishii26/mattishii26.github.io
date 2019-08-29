b11e2bd7a679a6ae19708559c2330b3730ed8dfe
mattishii26 <chihiro2657@gmail.com>
2019-08-29 00:16:58 -0700

    Updated Description

---
+++ precache-manifest.c0b4667d4d971a31a8e185c280e5a787.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdeb66dadab5814ea446",
+    "url": "/React/static/js/runtime~main.bdeb66da.js"
+  },
+  {
+    "revision": "151ce824db7ceae90820",
+    "url": "/React/static/js/main.151ce824.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "151ce824db7ceae90820",
+    "url": "/React/static/css/main.88467f2d.chunk.css"
+  },
+  {
+    "revision": "486f79dbe940b7c0ac67464e6b668c2d",
+    "url": "/React/index.html"
+  }
+];
