c23cb2133bfdd881196022600ce82a4a4395413c
mattishii26 <chihiro2657@gmail.com>
2019-09-03 23:45:53 -0700

    Fix jquery link

---
+++ precache-manifest.e03cc83b0741bc862f7c8da464293582.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdeb66dadab5814ea446",
+    "url": "/React/static/js/runtime~main.bdeb66da.js"
+  },
+  {
+    "revision": "c43f4570080d2727acf8",
+    "url": "/React/static/js/main.c43f4570.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "c43f4570080d2727acf8",
+    "url": "/React/static/css/main.88467f2d.chunk.css"
+  },
+  {
+    "revision": "77c9c5d0f9ee7e40cca63993fec8b444",
+    "url": "/React/index.html"
+  }
+];
