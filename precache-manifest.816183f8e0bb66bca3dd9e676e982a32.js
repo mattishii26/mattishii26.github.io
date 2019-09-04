5a72b905b75ab4acfff331b6cebacc5d780c8e00
mattishii26 <chihiro2657@gmail.com>
2019-09-03 23:40:37 -0700

    Revert

---
+++ precache-manifest.816183f8e0bb66bca3dd9e676e982a32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05e2b060c5d1d805f011",
+    "url": "/static/js/main.05e2b060.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "05e2b060c5d1d805f011",
+    "url": "/static/css/main.88467f2d.chunk.css"
+  },
+  {
+    "revision": "5e381a9418191c314e0d940ce1ad0a7d",
+    "url": "/index.html"
+  }
+];
