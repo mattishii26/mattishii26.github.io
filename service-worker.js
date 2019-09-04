7ec66a7b4122b9f523243ac01843ff4bbc95611b
mattishii26 <chihiro2657@gmail.com>
2019-09-03 23:28:02 -0700

    Added Flickr information
    Added Flickr information

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.816183f8e0bb66bca3dd9e676e982a32.js"
+  "/React/precache-manifest.1ad1b4c94afc5e1adc3dfd70fda47d07.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/React/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
