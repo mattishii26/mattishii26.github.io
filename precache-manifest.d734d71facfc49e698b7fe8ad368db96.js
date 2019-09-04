5383e9d3afb4cc5e131883c9fdea33178e5c9a4f
mattishii26 <chihiro2657@gmail.com>
2019-09-03 23:51:19 -0700

    Fix landing page

---
+++ precache-manifest.d734d71facfc49e698b7fe8ad368db96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "51e21f0e80610ac2f4df",
+    "url": "/static/js/main.51e21f0e.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "51e21f0e80610ac2f4df",
+    "url": "/static/css/main.88467f2d.chunk.css"
+  },
+  {
+    "revision": "d503c27a70644a476756d9dd799248a3",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React/precache-manifest.e03cc83b0741bc862f7c8da464293582.js"
+  "/precache-manifest.d734d71facfc49e698b7fe8ad368db96.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/React/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
